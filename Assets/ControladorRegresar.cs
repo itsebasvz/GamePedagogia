536dbd038f1438b0604ac56546f3dd48023de563
sebasgguwu <sebaskuhl2003@gmail.com>
2025-05-11 14:55:01 -0600

    Escena Introducción 2 Completa

--- Assets/ControladorRegresar.cs
+++ Assets/ControladorRegresar.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CambiarEscenaDosAUno : MonoBehaviour
+public class ControladorRegresar : MonoBehaviour
 {
     public void IrAlMenu()
     {
         SceneManager.LoadScene("MenuPrincipal"); // Asegúrate que el nombre sea exacto
     }
-    public void IrAIntroduccion()
-    {
-        SceneManager.LoadScene("Introduccion2"); // Asegúrate que el nombre sea exacto
-    }
 }
+
