350b5a6ba5fe944ed63762c048798d0de39fea62
Vichitore7 <calderav95@gmail.com>
2025-05-11 23:45:25 -0600

    Mision 3 completa

---
+++ Assets/JuegoComparacion.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JuegoComparacion : MonoBehaviour
+{
+    public GrupoFrutas grupo1;         // Arrástralo desde el Inspector
+    public GrupoFrutas grupo2;         // Igual
+    public Text feedbackText;          // Texto UI para mostrar el resultado
+
+    void Start()
+    {
+        // Establecer este manager como referencia en los grupos
+        grupo1.juegoManager = this;
+        grupo2.juegoManager = this;
+    }
+
+    public void ValidarSeleccion(GrupoFrutas seleccionado)
+    {
+        // Determinar cuál es el otro grupo
+        GrupoFrutas otro = (seleccionado == grupo1) ? grupo2 : grupo1;
+
+        // Comparar cantidades
+        if (seleccionado.cantidadFrutas > otro.cantidadFrutas)
+        {
+            feedbackText.text = "¡Genial! Has terminado por hoy. Estoy muy orgullosa de ti, ¡has\naprendido mucho hoy!";
+        }
+        else
+        {
+            feedbackText.text = "Inténtalo otra vez.";
+        }
+    }
+
+}
